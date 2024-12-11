16b54191365fff7677e0e0ec0dcc05d4d3485d70
tom.shephard <tom.shephard@audacia.co.uk>
2024-12-11 12:40:26 +0000

    test

--- src/Audacia.Azure.StorageQueue/AddMessageToQueue/IAddAzureQueueStorageService.cs
+++ src/Audacia.Azure.StorageQueue/AddMessageToQueue/IAddAzureQueueStorageService.cs
@@ -5,14 +5,7 @@ using Azure.Storage.Queues.Models;
 namespace Audacia.Azure.StorageQueue.AddMessageToQueue
 {
     /// <summary>
-    /// Add service for adding messages to an Azure Queue Storage account.
-    /// </summary>
-    public interface IAddAzureQueueStorageService
-    {
-        /// <summary>
-        /// Add a new message to a queue.
-        /// </summary>
-        /// <param name="queueName">Name of the queue which the message will be added too.</param>
+ssage will be added too.</param>
         /// <param name="queueMessage">String containing the context of the message.</param>
         /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>The receipt from adding a message to the queue.</returns>
